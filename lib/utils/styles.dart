4a7207fbf76673e87f012fabd432d8b2173b0217
ericlefyson <ericlefysonsantana@gmail.com>
2023-04-13 23:03:58 -0300

    Add colors of theme and change app name

---
+++ lib/utils/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(5, 188, 240, 1); 
+  static const Color secondary = Color.fromRGBO(20, 14, 17, 1);
+  static const Color darkPrimary = Color.fromRGBO(16, 58, 77, 1);
+  static const Color mediumPrimary = Color.fromRGBO(10, 117, 152, 1);
+  static const Color lightPrimary = Color.fromRGBO(14, 255, 249, 1);
+  static const Color lightWhite = Color.fromRGBO(237, 240, 240, 1);
+}
